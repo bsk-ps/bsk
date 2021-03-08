378c799ce4de3af9a7d258e2965360a3a5b6cfca
ssarosiek99 <sarosiekszymon@gmail.com>
2021-03-08 14:18:39 +0100

    Export to txt

--- ui/bsk/src/components/Card.js
+++ ui/bsk/src/components/Card.js
@@ -20,6 +20,7 @@ const InputCard = ({ form, setForm }) => {
             data: event.target.value,
         })
     }
+
     return (
         <div className="paper card">
             <h2 className="display-2">ENTER DATA</h2>
@@ -46,13 +47,29 @@ const InputCard = ({ form, setForm }) => {
 }
 
 const OutputCard = ({ output }) => {
+    const downloadToTxt = () =>{
+        const element = document.createElement("a");
+    
+        const file = new Blob([{output}.output], {type: 'text/plain; charset=utf-8'});
+        element.href = URL.createObjectURL(file);
+        element.download="data.txt";
+        document.body.appendChild(element);
+        element.click();
+    }
     return (
         <div className="paper card" style={{ whiteSpace: "pre-line" }}>
             <h2 className="display-2">OUTPUT</h2>
             <hr />
-            <div id="scrollable" style={{overflow: "auto", width: "100%", wordWrap: "break-word"}}>
+            <div id="scrollable" style={{height:"200px", overflow: "auto", width: "100%", wordWrap: "break-word"}}>
                 {output}
             </div>
+            <hr />
+            <h2 className="display-2">DOWNLOAD OUTPUT</h2>
+            <button  onClick={downloadToTxt} className="btn-primary" >
+            <span className="material-icons">
+                                download
+                            </span>
+                        </button>
         </div>
     );
 }
