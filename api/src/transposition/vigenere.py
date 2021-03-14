3ba44092a5dd4c45ce5d56b9383762d6198ff7f2
ssarosiek99 <sarosiekszymon@gmail.com>
2021-03-14 19:41:39 +0100

    fix vigenere cipher, vigenere cipher tests

--- api/src/transposition/vigenere.py
+++ api/src/transposition/vigenere.py
@@ -1,31 +1,45 @@
-def key_gen(text, key):
-    if (len(text) == len(key)):
+
+def key_gen(message: str, key: list) -> list:
+    if (len(message) == len(key)):
         return key
-    elif (len(text) > len(key)):
-        for i in range(len(text) - len(key)):
+    elif (len(message) > len(key)):
+        for i in range(len(message) - len(key)):
             key.append(key[i % len(key)])
         return key
     else:
         # key is too long
         return 0;
 
-def encode(text, key):
+def encode(message: str, key:str) -> str:
     cipher=[]
-    mod_key = key_gen(text, list(key))
+    mod_key = key_gen(message, list(key))
 
-    for i in range (len(text)):
-        offset = (ord(mod_key[i]) - ord('a'))
-        x = ord('a') + (ord(text[i]) - ord('a') + offset) %26
+    for i in range (len(message)):
+        if(message[i].isupper()):
+            offset = (ord(mod_key[i]) - ord('A'))
+            x = ord('A') + (ord(message[i]) - ord('A') + offset) % 26
+        else:
+
+          offset = (ord(mod_key[i]) - ord('a'))
+          x = ord('a') + (ord(message[i]) - ord('a') + offset) %26
         cipher.append(chr(x))
     return("".join(cipher))
 
 
-def decode(text, key):
+def decode(message: str, key: str) -> str:
     decipher = []
-    mod_key = key_gen(text, list(key))
-    for i in range(len(text)):
-        offset = (ord(mod_key[i]) + ord('a'))
-        x = ord('a') + (ord(text[i]) + ord('a') - offset) % 26
+    mod_key = key_gen(message, list(key))
+    for i in range(len(message)):
+        if(message[i].isupper()):
+            offset = (ord(mod_key[i]) + ord('A'))
+            x = ord('A') + (ord(message[i]) + ord('A') - offset) % 26
+        else:
+            offset = (ord(mod_key[i]) + ord('a'))
+            x = ord('a') + (ord(message[i]) + ord('a') - offset) % 26
         decipher.append(chr(x))
     return ("".join(decipher))
 
+
+if __name__ == "__main__":
+    print(encode("szymon", "fsda"))
+    print(decode("xrbmtf", "fsda"))
---
+++ api/tests/transposition/test_vigenere.py
@@ -0,0 +1,20 @@
+from src.transposition import vigenere
+
+examples = [
+    ("CRYPTOGRAPHY",
+     "GHRTA",
+     "IYPITUNITPNF",
+     ),
+]
+
+def test_encode():
+    for example in examples:
+        message, key, result = example
+        output = vigenere.encode(message, key)
+        assert output == result
+
+def test_decode():
+    for example in examples:
+        message, key, result = example
+        output = vigenere.decode(result, key)
+        assert output == message
