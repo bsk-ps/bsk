c9182287da0958edde0b5bb237e7b13294955904
ssarosiek99 <sarosiekszymon@gmail.com>
2021-03-27 17:02:01 +0100

    stream cipher encrypting

---
+++ api/src/lfsr/stream_cipher.py
@@ -0,0 +1,15 @@
+from lfsr import lfsr
+
+def encrypt(seed: str,text: str, polynomial: tuple):
+    cipher=[]
+    sum=0
+    stream = lfsr(seed,polynomial,len(text))
+    for i in range(len(stream)):
+        sum+= int(stream[i] + text[i])
+        if(sum%2==0):
+            cipher.append('0')
+        else:
+            cipher.append('1')
+        sum=0
+    return "".join(cipher)
+
