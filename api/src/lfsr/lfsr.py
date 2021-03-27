63f2413bbebb56d78d9001b262bdb4f5b42d8f79
ssarosiek99 <sarosiekszymon@gmail.com>
2021-03-27 15:37:10 +0100

    change lfsr return

--- api/src/lfsr/lfsr.py
+++ api/src/lfsr/lfsr.py
@@ -1,3 +1,4 @@
+""""
 def lfsr(seed: str, polynomial: tuple, n: int):
     lfsr = list(seed)
     sum=0
@@ -12,6 +13,31 @@ def lfsr(seed: str, polynomial: tuple, n: int):
         lfsr.insert(0,'1')
      yield lfsr.pop()
      sum=0
+
+
+"""
+
+
+def lfsr(seed: str, polynomial: tuple, n: int):
+    lfsr = list(seed)
+    cipher=[]
+    sum=0
+
+    for i in range(n):
+     for i in range(len(lfsr)):
+        if (i+1) in polynomial:
+          sum += int(lfsr[i])
+     if(sum% 2 ==0):
+        lfsr.insert(0,'0')
+     else:
+        lfsr.insert(0,'1')
+     cipher.append(lfsr.pop())
+     sum=0
+    return cipher
+
+
+if __name__ == "__main__":
+    print(lfsr("1011011",(8,7,6,1),50))
 
 
 
